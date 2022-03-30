c6ebbd0447ffbd309d44365f7d85b0b39ed7def0
alejandro-santos <alesantos319@gmail.com>
2022-03-30 19:33:39 -0400

    Updates

---
+++ precache-manifest.9cd56ae7784367908808c89d75b8544e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "713fe64127fcca8573ac",
+    "url": "/todo-list/static/css/main.c67bd61b.chunk.css"
+  },
+  {
+    "revision": "713fe64127fcca8573ac",
+    "url": "/todo-list/static/js/main.713fe641.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/todo-list/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "0bbd8ea5f85b3a7b208c",
+    "url": "/todo-list/static/js/2.0bbd8ea5.chunk.js"
+  },
+  {
+    "revision": "4d7ed81404ef102a4875",
+    "url": "/todo-list/static/js/runtime~main.4d7ed814.js"
+  },
+  {
+    "revision": "9c99ed7451a132a9c3ad7c7e68af195c",
+    "url": "/todo-list/index.html"
+  }
+];
